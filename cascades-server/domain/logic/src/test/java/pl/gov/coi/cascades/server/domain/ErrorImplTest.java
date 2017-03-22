49a9d390a520ad8accc82f81f7ae7b4402dd517c
Agnieszka Celuch <agnieszka.celuch@coi.gov.pl>
2017-03-22 15:11:26 +0100

    PRS-2785 Adding test to ErrorImpl

---
+++ cascades-server/domain/logic/src/test/java/pl/gov/coi/cascades/server/domain/ErrorImplTest.java
@@ -0,0 +1,42 @@
+package pl.gov.coi.cascades.server.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author <a href="agnieszka.celuch@coi.gov.pl">Agnieszka Celuch</a>
+ * @since 22.03.17.
+ */
+public class ErrorImplTest {
+
+    private ErrorImpl error;
+
+    @Before
+    public void setUp() {
+        error = new ErrorImpl(
+            "Error occurred.",
+            "testPath"
+        );
+    }
+
+    @Test
+    public void testGetMessage() throws Exception {
+        // when
+        String actual = error.getMessage();
+
+        // then
+        assertThat(actual).isEqualTo("Error occurred.");
+    }
+
+    @Test
+    public void testGetPropertyPath() throws Exception {
+        // when
+        String actual = error.getPropertyPath();
+
+        // then
+        assertThat(actual).isEqualTo("testPath");
+    }
+
+}
