54afc7f6f37f2b6becedbdeed6c229b5e54b9e88
Agnieszka Celuch <agnieszka.celuch@coi.gov.pl>
2017-04-26 12:26:01 +0200

    PRS-2860 Adding tests to template suppliers

---
+++ cascades-server/persistence-hibernate/src/test/java/pl/gov/coi/cascades/server/persistance/hibernate/development/supplier/template/F4ab6a58SupplierTest.java
@@ -0,0 +1,34 @@
+package pl.gov.coi.cascades.server.persistance.hibernate.development.supplier.template;
+
+import org.junit.Test;
+import pl.gov.coi.cascades.server.persistance.hibernate.entity.TemplateId;
+import pl.gov.coi.cascades.server.persistance.hibernate.entity.TemplateIdStatus;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author <a href="agnieszka.celuch@coi.gov.pl">Agnieszka Celuch</a>
+ * @since 26.04.17.
+ */
+public class F4ab6a58SupplierTest {
+
+    @Test
+    public void testGet() throws Exception {
+        // given
+        TemplateIdStatus status = TemplateIdStatus.CREATED;
+        String name = "postgres_template";
+        String serverId = "dgrt45gtyt";
+        F4ab6a58Supplier f4ab6a58Supplier = new F4ab6a58Supplier();
+
+        // when
+        TemplateId actual = f4ab6a58Supplier.get();
+
+        // then
+        assertThat(actual).isNotNull();
+        assertThat(actual.getStatus()).isEqualTo(status);
+        assertThat(actual.getName()).isEqualTo(name);
+        assertThat(actual.getServerId()).isEqualTo(serverId);
+        assertThat(actual.isDefault()).isFalse();
+    }
+
+}
