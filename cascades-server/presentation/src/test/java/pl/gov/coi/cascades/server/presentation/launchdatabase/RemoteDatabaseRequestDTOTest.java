6df9a455a66c608ad342b437be35eb720da0c697
Agnieszka Celuch <agnieszka.celuch@coi.gov.pl>
2017-04-28 08:40:57 +0200

    PRS-2860 Adding test to RemoteDatabaseRequestDTO

--- cascades-server/presentation/src/test/java/pl/gov/coi/cascades/server/presentation/launchdatabase/RemoteDatabaseRequestDTOTest.java
+++ cascades-server/presentation/src/test/java/pl/gov/coi/cascades/server/presentation/launchdatabase/RemoteDatabaseRequestDTOTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RemoteDatabaseRequestDTOTest {
 
     @Test
-    public void testParameterConstructor() {
+    public void testParameterConstructorWhenTemplateIdIsNotPresent() {
         // given
         String type = "";
         String instanceName = "oracler49903";
@@ -26,6 +26,27 @@ public class RemoteDatabaseRequestDTOTest {
         // then
         assertThat(actual).isNotNull();
         assertThat(actual.getTemplateId()).isEqualTo(com.google.common.base.Optional.absent());
+    }
+
+    @Test
+    public void testParameterConstructorWhenTemplateIdIsPresent() {
+        // given
+        String type = "";
+        String instanceName = "oracler49903";
+        String templateId = "oracle_template";
+
+        // when
+        RemoteDatabaseRequestDTO actual = new RemoteDatabaseRequestDTO(
+            type,
+            templateId,
+            instanceName
+        );
+
+        // then
+        assertThat(actual).isNotNull();
+        assertThat(actual.getTemplateId().isPresent()).isTrue();
+        assertThat(actual.getTemplateId().get().getId()).isEqualTo(templateId);
+        assertThat(actual.getTemplateId().get()).isInstanceOf(InputTemplateId.class);
     }
 
 }
