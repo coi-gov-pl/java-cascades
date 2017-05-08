ee13a72b9735e109813b08679ac34273b58e9b3c
Agnieszka Celuch <agnieszka.celuch@coi.gov.pl>
2017-05-08 13:01:19 +0200

    PRS-2860 Adding test to UserGatewayImpl

--- cascades-server/persistence-hibernate/src/test/java/pl/gov/coi/cascades/server/persistance/hibernate/UserGatewayImplTest.java
+++ cascades-server/persistence-hibernate/src/test/java/pl/gov/coi/cascades/server/persistance/hibernate/UserGatewayImplTest.java
@@ -2,6 +2,7 @@ package pl.gov.coi.cascades.server.persistance.hibernate;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
@@ -9,12 +10,19 @@ import pl.gov.coi.cascades.server.domain.DatabaseTypeClassNameService;
 import pl.gov.coi.cascades.server.domain.User;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
+import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.contains;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * @author <a href="agnieszka.celuch@coi.gov.pl">Agnieszka Celuch</a>
@@ -33,6 +41,9 @@ public class UserGatewayImplTest {
 
     @Rule
     public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Test
     public void testSave() throws Exception {
@@ -56,6 +67,64 @@ public class UserGatewayImplTest {
         // then
         verify(entityManager, times(1)).getReference(any(), anyLong());
         verify(entityManager, times(1)).persist(any());
+    }
+
+    @Test
+    public void testFind() throws Exception {
+        // given
+        String username = "Kendrick Lamar";
+        String id = "123456789";
+        String email = "klamar@example.org";
+        pl.gov.coi.cascades.server.persistance.hibernate.entity.User user = new pl.gov.coi.cascades.server.persistance.hibernate.entity.User();
+        user.setUsername(username);
+        user.setId(Long.parseLong(id));
+        user.setEmail(email);
+
+        UserGatewayImpl userGateway = new UserGatewayImpl(
+            databaseTypeClassNameService
+        );
+        userGateway.setEntityManager(entityManager);
+        when(entityManager.createQuery(anyString(), any())).thenReturn(query);
+        when(query.setParameter(anyString(), anyString())).thenReturn(query);
+        when(query.setMaxResults(anyInt())).thenReturn(query);
+        when(query.getSingleResult()).thenReturn(user);
+
+        // when
+        Optional<User> actual = userGateway.find(username);
+
+        // then
+        assertThat(actual.isPresent()).isTrue();
+        assertThat(actual.get().getUsername()).isEqualTo(username);
+        assertThat(actual.get().getId()).isEqualTo(id);
+        assertThat(actual.get().getEmail()).isEqualTo(email);
+    }
+
+    @Test
+    public void testFindWhenExceptionOccurred() throws Exception {
+        // given
+        String username = "Kendrick Lamar";
+        String id = "123456789";
+        String email = "klamar@example.org";
+        pl.gov.coi.cascades.server.persistance.hibernate.entity.User user = new pl.gov.coi.cascades.server.persistance.hibernate.entity.User();
+        user.setUsername(username);
+        user.setId(Long.parseLong(id));
+        user.setEmail(email);
+
+        UserGatewayImpl userGateway = new UserGatewayImpl(
+            databaseTypeClassNameService
+        );
+        userGateway.setEntityManager(entityManager);
+        when(entityManager.createQuery(anyString(), any())).thenReturn(query);
+        when(query.setParameter(anyString(), anyString())).thenReturn(query);
+        when(query.setMaxResults(anyInt())).thenReturn(query);
+        NoResultException exception = new NoResultException();
+        when(query.getSingleResult()).thenThrow(exception);
+
+        // then
+        expectedException.expectMessage(contains("20170329:171038"));
+
+        // when
+        userGateway.find(username);
     }
 
 }
