f28316bf39f2c9ca7651f6320975b1adbec9b2a7
Agnieszka Celuch <agnieszka.celuch@coi.gov.pl>
2017-05-08 16:01:11 +0200

    PRS-2860 Adding test to UserGatewayImpl

--- cascades-server/persistence-hibernate/src/test/java/pl/gov/coi/cascades/server/persistance/hibernate/UserGatewayImplTest.java
+++ cascades-server/persistence-hibernate/src/test/java/pl/gov/coi/cascades/server/persistance/hibernate/UserGatewayImplTest.java
@@ -6,6 +6,7 @@ import org.junit.rules.ExpectedException;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
+import org.slf4j.Logger;
 import pl.gov.coi.cascades.server.domain.DatabaseTypeClassNameService;
 import pl.gov.coi.cascades.server.domain.User;
 
@@ -38,6 +39,9 @@ public class UserGatewayImplTest {
 
     @Mock
     private TypedQuery<Object> query;
+
+    @Mock
+    private Logger logger;
 
     @Rule
     public MockitoRule mockitoRule = MockitoJUnit.rule();
@@ -112,19 +116,20 @@ public class UserGatewayImplTest {
         user.setEmail(email);
 
         UserGatewayImpl userGateway = new UserGatewayImpl(
-            databaseTypeClassNameService
+            databaseTypeClassNameService,
+            logger
         );
         userGateway.setEntityManager(entityManager);
-        when(entityManager.createQuery(anyString(), any())).thenReturn(query);
+        when(entityManager.createQuery(anyString(), any())).thenThrow(exception);
         when(query.setParameter(anyString(), anyString())).thenReturn(query);
         when(query.setMaxResults(anyInt())).thenReturn(query);
-        when(query.getSingleResult()).thenThrow(exception);
-
-        // then
-        expectedException.expectMessage(contains("20170329:171038"));
 
         // when
-        userGateway.find(username);
+        Optional<User> actual = userGateway.find(username);
+
+        // then
+        assertThat(actual.isPresent()).isFalse();
+        verify(logger, times(1)).error(contains("20170329:171038"));
     }
 
 }
