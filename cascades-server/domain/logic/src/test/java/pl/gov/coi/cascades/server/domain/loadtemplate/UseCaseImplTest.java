8938f0c13d80c7f046ae2706000e5d7de532d3d8
aceluch <agnieszka.celuch@coi.gov.pl>
2017-06-08 09:26:58 +0200

    PRS-2835 Adding test to UseCaseImpl

---
+++ cascades-server/domain/logic/src/test/java/pl/gov/coi/cascades/server/domain/loadtemplate/UseCaseImplTest.java
@@ -0,0 +1,93 @@
+package pl.gov.coi.cascades.server.domain.loadtemplate;
+
+import lombok.Getter;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import pl.gov.coi.cascades.contract.service.Violation;
+
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.HashSet;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author <a href="agnieszka.celuch@coi.gov.pl">Agnieszka Celuch</a>
+ * @since 07.06.17.
+ */
+public class UseCaseImplTest {
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void testBuilder() throws Exception {
+        // when
+        UseCaseImpl useCase = UseCaseImpl.builder()
+            .build();
+
+        // then
+        assertThat(useCase).isNotNull();
+    }
+
+    private static final class ResponseImpl implements Response {
+
+        @Getter
+        private final Collection<Violation> violations = new HashSet<>();
+        @Getter
+        private String id;
+        @Getter
+        private String status;
+        @Getter
+        private boolean isDefault;
+        @Getter
+        private String serverId;
+        @Getter
+        private String version;
+
+        @Override
+        public void addViolation(Violation violation) {
+            violations.add(violation);
+        }
+
+        @Override
+        public boolean isSuccessful() {
+            return violations.isEmpty();
+        }
+
+        @Override
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        @Override
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        @Override
+        public void setDefault(boolean isDefault) {
+            this.isDefault = isDefault;
+        }
+
+        @Override
+        public void setServerId(String versionId) {
+            this.serverId = serverId;
+        }
+
+        @Override
+        public void setVersion(String version) {
+            this.version = version;
+        }
+    }
+
+}
