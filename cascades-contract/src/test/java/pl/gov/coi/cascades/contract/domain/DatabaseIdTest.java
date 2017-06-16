64d7141e0361fad3aa7f20846be89e5c04367b98
aceluch <agnieszka.celuch@coi.gov.pl>
2017-06-16 12:45:34 +0200

    PRS-Adding test to DatabaseId

--- cascades-contract/src/test/java/pl/gov/coi/cascades/contract/domain/DatabaseIdTest.java
+++ cascades-contract/src/test/java/pl/gov/coi/cascades/contract/domain/DatabaseIdTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author <a href="agnieszka.celuch@coi.gov.pl">Agnieszka Celuch</a>
@@ -16,6 +17,31 @@ public class DatabaseIdTest {
     @Before
     public void setUp() {
         id = "1234567abcd";
+    }
+
+    @Test
+    public void testToString() {
+        // given
+        DatabaseId databaseId = new DatabaseId(id);
+
+        // when
+        String actual = databaseId.toString();
+
+        // then
+        assertThat(actual).isEqualTo(databaseId.toString());
+    }
+
+
+    @Test
+    public void testGetter() {
+        // given
+        DatabaseId databaseId = new DatabaseId(id);
+
+        // when
+        String actual = databaseId.getId();
+
+        // then
+        assertThat(actual).isEqualTo(id);
     }
 
     @Test
