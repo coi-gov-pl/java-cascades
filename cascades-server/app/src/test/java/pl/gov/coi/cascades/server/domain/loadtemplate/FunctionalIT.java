c4e46ab59f2c06e6739c49b1af6d080afec06314
aceluch <agnieszka.celuch@coi.gov.pl>
2017-06-26 11:13:19 +0200

    PRS-2835 Renaming test

--- cascades-server/app/src/test/java/pl/gov/coi/cascades/server/domain/loadtemplate/FunctionalIT.java
+++ cascades-server/app/src/test/java/pl/gov/coi/cascades/server/domain/loadtemplate/FunctionalIT.java
@@ -42,7 +42,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @StubDevelopmentTest
-public class Functional {
+public class FunctionalIT {
 
     private String path;
     private static final String TEST = "src/test/resources";
