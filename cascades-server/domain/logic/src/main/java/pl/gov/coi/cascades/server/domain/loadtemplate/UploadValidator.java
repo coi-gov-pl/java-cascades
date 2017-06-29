f1ba0fe38d518f0327b6fdd9585bd7086f4f51fa
aceluch <agnieszka.celuch@coi.gov.pl>
2017-06-29 15:36:07 +0200

    PRS-2835 Adding tests to UploadValidator

--- cascades-server/domain/logic/src/main/java/pl/gov/coi/cascades/server/domain/loadtemplate/UploadValidator.java
+++ cascades-server/domain/logic/src/main/java/pl/gov/coi/cascades/server/domain/loadtemplate/UploadValidator.java
@@ -70,4 +70,5 @@ class UploadValidator extends AbstractListenableValidator {
             )
         );
     }
+
 }
---
+++ cascades-server/domain/logic/src/test/java/pl/gov/coi/cascades/server/domain/loadtemplate/UnzippedValidatorTest.java
@@ -0,0 +1,14 @@
+package pl.gov.coi.cascades.server.domain.loadtemplate;
+
+import org.junit.Test;
+
+/**
+ * @author <a href="agnieszka.celuch@coi.gov.pl">Agnieszka Celuch</a>
+ * @since 29.06.17
+ */
+public class UnzippedValidatorTest {
+    @Test
+    public void isValid() throws Exception {
+    }
+
+}
