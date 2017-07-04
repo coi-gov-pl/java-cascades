d00d87b6fb32827f93492db511c7ddf9ecfee506
aceluch <agnieszka.celuch@coi.gov.pl>
2017-07-04 15:06:08 +0200

    PRS-2935 Adding implementation to CascadesOperationsLoggerImpl

--- cascades-client/src/main/java/pl/gov/coi/cascades/client/presentation/CascadesOperationsLoggerImpl.java
+++ cascades-client/src/main/java/pl/gov/coi/cascades/client/presentation/CascadesOperationsLoggerImpl.java
@@ -1,8 +1,12 @@
 package pl.gov.coi.cascades.client.presentation;
 
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pl.gov.coi.cascades.client.service.ServiceExecutionContext;
 import pl.gov.coi.cascades.contract.service.RemoteDatabaseSpec;
 import pl.gov.coi.cascades.contract.service.Violation;
+import pl.wavesoftware.eid.exceptions.Eid;
 
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
@@ -11,20 +15,46 @@ import java.util.concurrent.TimeoutException;
  * @author <a href="mailto:krzysztof.suszynski@coi.gov.pl">Krzysztof Suszynski</a>
  * @since 30.03.17
  */
+@AllArgsConstructor
 class CascadesOperationsLoggerImpl implements CascadesOperationsLogger {
+
+    private static final Logger DEFAULT_LOGGER = LoggerFactory.getLogger(CascadesOperationsLoggerImpl.class);
+    private Logger logger;
+
+    CascadesOperationsLoggerImpl() {
+        this(DEFAULT_LOGGER);
+    }
+
     @Override
     public void logDatabaseCreated(RemoteDatabaseSpec spec) {
-        // TODO: write some implementation here ;-P
+        if (logger.isInfoEnabled()) {
+            logger.info(new Eid("20170704:144802").makeLogMessage(
+                "Database: " +
+                    spec.toString() +
+                    " has been created successfully."
+            ));
+        }
     }
 
     @Override
     public void logDatabaseCreateFailed(ExecutionException ex) {
-        // TODO: write some implementation here ;-P
+        if (logger.isErrorEnabled()) {
+            logger.error(new Eid("20170704:145034").makeLogMessage(
+                "Database has not been created because of: " +
+                    ex
+            ));
+        }
     }
 
     @Override
     public void logDatabaseRemoved(RemoteDatabaseSpec spec) {
-        // TODO: write some implementation here ;-P
+        if (logger.isInfoEnabled()) {
+            logger.info(new Eid("20170704:145551").makeLogMessage(
+                "Database: " +
+                    spec.toString() +
+                    " has been deleted successfully."
+            ));
+        }
     }
 
     @Override
@@ -40,6 +70,15 @@ class CascadesOperationsLoggerImpl implements CascadesOperationsLogger {
     @Override
     public void logDatabaseCreateFailed(ServiceExecutionContext<?> executionContext,
                                         Iterable<Violation> violations) {
-        // TODO: write some implementation here ;-P
+        if (logger.isErrorEnabled()) {
+            logger.error(new Eid("20170704:145935").makeLogMessage(
+                "Request: " +
+                    executionContext.getRequest() +
+                    " with operation: " +
+                    executionContext.getOperationName() +
+                    " failed because of business logic violations: " +
+                    violations.toString()
+            ));
+        }
     }
 }
--- cascades-contract/src/main/java/pl/gov/coi/cascades/contract/service/RemoteDatabaseSpec.java
+++ cascades-contract/src/main/java/pl/gov/coi/cascades/contract/service/RemoteDatabaseSpec.java
@@ -16,40 +16,41 @@ public class RemoteDatabaseSpec implements Serializable {
 
     private static final long serialVersionUID = 42L;
 
-	@Getter
-	private final DatabaseId databaseId;
-	@Getter
-	private final String databaseName;
-	@Getter
-	private final NetworkBind networkBind;
-	@Getter
-	private final UsernameAndPasswordCredentials credentials;
+    @Getter
+    private final DatabaseId databaseId;
+    @Getter
+    private final String databaseName;
+    @Getter
+    private final NetworkBind networkBind;
+    @Getter
+    private final UsernameAndPasswordCredentials credentials;
 
-	/**
-	 * Required argument constructor.
+    /**
+     * Required argument constructor.
      *
-	 * @param databaseId Given id of template.
-	 * @param databaseName Given name of database.
-	 * @param networkBind Given network bind.
-	 * @param credentials Given credentials.
-	 */
-	public RemoteDatabaseSpec(DatabaseId databaseId,
-							  String databaseName,
-							  NetworkBind networkBind,
-							  UsernameAndPasswordCredentials credentials) {
-		this.databaseId = databaseId;
-		this.databaseName = databaseName;
-		this.networkBind = networkBind;
-		this.credentials = credentials;
-	}
+     * @param databaseId   Given id of template.
+     * @param databaseName Given name of database.
+     * @param networkBind  Given network bind.
+     * @param credentials  Given credentials.
+     */
+    public RemoteDatabaseSpec(DatabaseId databaseId,
+                              String databaseName,
+                              NetworkBind networkBind,
+                              UsernameAndPasswordCredentials credentials) {
+        this.databaseId = databaseId;
+        this.databaseName = databaseName;
+        this.networkBind = networkBind;
+        this.credentials = credentials;
+    }
 
     /**
      * Returns a valid connection string to be used in JDBC
+     *
      * @return a valid connection string
      */
-	public String getConnectionString(DatabaseType type) {
+    public String getConnectionString(DatabaseType type) {
         ConnectionStringProducer producer = type.getConnectionStringProducer();
         return producer.produce(networkBind, databaseName);
-	}
+    }
 
 }
