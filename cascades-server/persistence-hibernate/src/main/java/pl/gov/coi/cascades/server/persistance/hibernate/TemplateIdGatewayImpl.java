8c75ba1a5db8c9d16f98595a07812eb364a178b9
Lukasz M <40563254+labamba63@users.noreply.github.com>
2018-07-04 16:02:05 +0200

    bugfix/fix-save-information-about-new-template add persist template (#24)
    *  add persist template to database
    * refator test: TemplateGatewayImplFunctionalIT and add new test

--- cascades-server/persistence-hibernate/src/main/java/pl/gov/coi/cascades/server/persistance/hibernate/TemplateIdGatewayImpl.java
+++ cascades-server/persistence-hibernate/src/main/java/pl/gov/coi/cascades/server/persistance/hibernate/TemplateIdGatewayImpl.java
@@ -25,7 +25,7 @@ public class TemplateIdGatewayImpl implements TemplateIdGateway {
 
     private static final Logger DEFAULT_LOGGER = LoggerFactory.getLogger(TemplateIdGatewayImpl.class);
     private static final TemplateIdMapper DEFAULT_TEMPLATE_ID_MAPPER = new TemplateIdMapper();
-    private static final String TEMPLATE_ID_FIELD = "templateIdAsLong";
+    private static final String TEMPLATE_ID_FIELD = "templateId";
     private static final int RADIX_36 = 36;
     private EntityManager entityManager;
     private Logger logger;
@@ -40,30 +40,27 @@ public class TemplateIdGatewayImpl implements TemplateIdGateway {
 
     @VisibleForTesting
     public TemplateIdGatewayImpl(TemplateIdMapper templateIdMapper,
-                          Logger logger) {
+                                 Logger logger) {
         this.templateIdMapper = templateIdMapper;
         this.logger = logger;
     }
 
     @PersistenceContext
-    void setEntityManager(EntityManager entityManager) {
+    public void setEntityManager(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
 
     @Override
     public Optional<pl.gov.coi.cascades.contract.domain.Template> find(@Nullable String templateId) {
         try {
-            Long templateIdAsLong = templateId != null
-                ? Long.parseLong(templateId, RADIX_36)
-                : null;
             TypedQuery<Template> query =
                 entityManager.createQuery(
                     "SELECT template FROM Template template " +
-                        "WHERE template.id = :templateIdAsLong",
+                        "WHERE template.generatedId = :templateId",
                     Template.class
                 )
-                .setParameter(TEMPLATE_ID_FIELD, templateIdAsLong)
-                .setMaxResults(1);
+                    .setParameter(TEMPLATE_ID_FIELD, templateId)
+                    .setMaxResults(1);
 
             return Optional.of(templateIdMapper.fromHibernateEntity(query.getSingleResult()));
         } catch (NoResultException e) {
@@ -87,7 +84,7 @@ public class TemplateIdGatewayImpl implements TemplateIdGateway {
                         "WHERE template.isDefault = true",
                     Template.class
                 )
-                .setMaxResults(1);
+                    .setMaxResults(1);
 
             return Optional.of(templateIdMapper.fromHibernateEntity(query.getSingleResult()));
         } catch (NoResultException e) {
@@ -103,6 +100,8 @@ public class TemplateIdGatewayImpl implements TemplateIdGateway {
 
     @Override
     public void addTemplate(pl.gov.coi.cascades.contract.domain.Template template) {
+        Template hibernateTemplate = templateIdMapper.toHibernateEntity(template);
+        entityManager.persist(hibernateTemplate);
         if (logger.isInfoEnabled()) {
             logger.info(new Eid("20170626:140337")
                 .makeLogMessage(
