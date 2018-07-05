f239d2a6919dc2196e4db4c90951ad5a5260e392
Lukasz Malek <lukasz.malek@coi.gov.pl>
2018-07-05 08:22:49 +0200

    feature/create-database-instance-save-in-h2-developer-mode remove imports

--- cascades-server/app/src/test/java/pl/gov/coi/cascades/server/TemplateGatewayImplFunctionalIT.java
+++ cascades-server/app/src/test/java/pl/gov/coi/cascades/server/TemplateGatewayImplFunctionalIT.java
@@ -1,35 +1,17 @@
 package pl.gov.coi.cascades.server;
 
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoRule;
-import org.slf4j.Logger;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import pl.gov.coi.cascades.contract.domain.Template;
 import pl.gov.coi.cascades.contract.domain.TemplateIdStatus;
 import pl.gov.coi.cascades.server.domain.TemplateIdGateway;
-import pl.gov.coi.cascades.server.domain.User;
-import pl.gov.coi.cascades.server.domain.UserGateway;
-import pl.gov.coi.cascades.server.persistance.hibernate.TemplateIdGatewayImpl;
-import pl.gov.coi.cascades.server.persistance.hibernate.mapper.TemplateIdMapper;
 
 import javax.inject.Inject;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.contains;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 /**
  * @author <a href="agnieszka.celuch@coi.gov.pl">Agnieszka Celuch</a>
